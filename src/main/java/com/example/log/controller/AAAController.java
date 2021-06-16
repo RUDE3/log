48f366f88ba4b5ea4615a682826e3b49108cf265
RUDE3 <913412672@qq.com>
2021-06-16 16:42:33 +0800

    log git hot-fix commit2

--- src/main/java/com/example/log/controller/AAAController.java
+++ src/main/java/com/example/log/controller/AAAController.java
@@ -6,5 +6,6 @@ public class AAAController {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hello git5");
     }
 }
